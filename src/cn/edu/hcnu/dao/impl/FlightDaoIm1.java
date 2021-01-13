26ca56c8dc346ae11a1a22302b61f31f0e1fc79e
Administrator <3110235580@qq.com>
2021-01-13 11:52:45 +0800

    1.2021-01-13首次提交 2.在jdk中添加jar包 3.在FlightDaolml中添加相关代码以连接数据库

--- src/cn/edu/hcnu/dao/impl/FlightDaoIm1.java
+++ src/cn/edu/hcnu/dao/impl/FlightDaoIm1.java
@@ -3,12 +3,33 @@ package cn.edu.hcnu.dao.impl;
 import cn.edu.hcnu.bean.Flight;
 import cn.edu.hcnu.dao.IFlightDao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Set;
 
 public class FlightDaoIm1 implements IFlightDao {
     @Override
-    public void insertFlight(Flight flight) {
+    public void insertFlight(Flight flight) throws SQLException {
 
+        //插入数据库
+        String url="jdbc:oracle:thin:@localhost:1521:orcl";//服务里的是OracleServiceORCL，所以最后写的是orcl而不是老师写的orclhc
+        String username="opts";
+        String password="opts12234";
+        Connection conn= DriverManager.getConnection(url,username,password);
+        String sql="INSERT INTO flight VALUES(?,?,?,?,?,?,?)";
+
+        PreparedStatement pstmt=conn.prepareStatement(sql);
+        pstmt.setString(1,flight.getId());
+        pstmt.setString(2,flight.getFlightId());
+        pstmt.setString(3,flight.getPlaneType());
+        pstmt.setInt(4,flight.getCurrentseatNumb());
+        pstmt.setString(5,flight.getDepartureAirPort());
+        pstmt.setString(6,flight.getDestinationAirPort());
+        pstmt.setString(7,flight.getDepartureTime());
+
+        pstmt.executeLargeUpdate();
     }
 
     @Override
