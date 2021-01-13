2318609223e5cd2137d8a0f62c525a705964e1c8
Administrator <3110235580@qq.com>
2021-01-13 16:16:59 +0800

    1.2021-01-13第三次提交 2.在MainUI中加入正则表达式匹配并获取错误信息的内容，以下以获取以"ORA-12899"开头的错误信息为例

--- src/cn/edu/hcnu/ui/MainUI.java
+++ src/cn/edu/hcnu/ui/MainUI.java
@@ -3,57 +3,75 @@ package cn.edu.hcnu.ui;
 import cn.edu.hcnu.bean.Flight;
 import cn.edu.hcnu.bll.IFlightService;
 import cn.edu.hcnu.bll.impl.FlightServiceImpl;
-import cn.edu.hcnu.dao.IFlightDao;
 
 import java.sql.SQLException;
 import java.util.Scanner;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainUI {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        while (true){
-            System.out.println("请输入相应数字进行操作：");
+        Scanner sc = new Scanner(System.in);//接受键盘输入
+        while (true) {
+            System.out.println("请输入相应的数字进行操作：");
+
             System.out.println("按1，录入航班信息");
             System.out.println("按2，显示所有航班信息");
             System.out.println("按3，查询航班信息");
-            System.out.println("按4，机票预定");
+            System.out.println("按4，机票预订");
             System.out.println("按5，机票退订");
             System.out.println("按6，退出系统");
 
-            int choice=sc.nextInt();
+            int choice = sc.nextInt();
 
-            if (choice == 1){
+            if (choice == 1) {
+                String id = UUID.randomUUID().toString().replace("-", "");
 
-                String id= UUID.randomUUID().toString();
-                System.out.println(id.replace("-",""));
-                System.out.println("请输入航班号：");
-                String flightId=sc.next();
-                System.out.println("请输入机型：");
-                String planeType=sc.next();
-                System.out.println("请输入座位数：");
-                int currentseatNumb=sc.nextInt();
-                System.out.println("请输入起飞机场：");
-                String departureAirPort=sc.next();
-                System.out.println("请输入目的机场：");
-                String destinationAirPort=sc.next();
-                System.out.println("请输入起飞时间：");
-                String departureTime=sc.next();
+                System.out.print("请输入航班编号：");
+                String flightId = sc.next();
+                System.out.print("请输入机型：");
+                String planeType = sc.next();
+                System.out.print("请输入座位数：");
+                int currentSeatsNum = sc.nextInt();
+                System.out.print("请输入起飞机场：");
+                String departureAirPort = sc.next();
+                System.out.print("请输入目的机场：");
+                String destinationAirPort = sc.next();
+                System.out.print("请输入起飞时间：");
+                String departureTime = sc.next();
 
-                Flight flight=new Flight(id,flightId,planeType,currentseatNumb,departureAirPort,
-                        destinationAirPort,departureTime);
+                Flight flight = new Flight(id, flightId, planeType, currentSeatsNum,
+                        departureAirPort, destinationAirPort, departureTime);
 
-                //异常
-                IFlightService iFlightService=new FlightServiceImpl();
-                try{
+                IFlightService iFlightService = new FlightServiceImpl();
+                try {
                     iFlightService.insertFlight(flight);
-                }catch (SQLException e){
-                    String errorMessage=e.getMessage();
-                    System.out.println(errorMessage);
-                    String errorId=errorMessage.substring(0,9);
-                    System.out.println("错误编号："+errorId);
-                    if ("ORA-12899".equals(errorId)){
-                        System.out.println("某列的值过大，请仔细检查");
+                } catch (SQLException e) {
+                    String errorMessage = e.getMessage();
+                    //正则表达式匹配并获取错误信息的内容，以下以获取以"ORA-12899"开头的错误信息为例
+                    if (errorMessage.startsWith("ORA-12899")) {
+                        //ORA-12899: value too large for column "OPTS"."FLIGHT"."ID" (actual: 32, maximum: 30)
+                        // 按指定模式在字符串查找
+                        //\\w:匹配字母、数字、下划线。等价于 [A-Za-z0-9_]
+                        //+:匹配前面的子表达式一次或多次。例如，'zo+' 能匹配 "zo" 以及 "zoo"，但不能匹配 "z"。+ 等价于 {1,}。
+                        //-、：没有规则直接写原有的
+                        //\\d{5}:表示五个数字，其中d表示digit是数字
+                        //（）起来的部分表示一组
+                        //\\s:表示空格
+                        //\\.表示.,因为.本身存在	匹配除换行符 \n 之外的任何单字符。要匹配 . ，请使用 \. 意义所以需要两个\表示转义
+                        String pattern = "(\\w+-\\d{5}):(\\s\\w+)+\\s(\"\\w+\")\\.(\"\\w+\")\\.(\"\\w+\")";
+                        // 创建 Pattern 对象
+                        Pattern r = Pattern.compile(pattern);
+                        // 现在创建 matcher 对象
+                        Matcher m = r.matcher(errorMessage);
+                        if (m.find()) {
+                            String tableName = m.group(4);
+                            String columnName = m.group(5);
+                            System.out.println(tableName + "表的" + columnName + "这一列的值过大，请仔细检查");
+                        } else {
+                            System.out.println("NO MATCH");
+                        }
                     }
                 }
             }
